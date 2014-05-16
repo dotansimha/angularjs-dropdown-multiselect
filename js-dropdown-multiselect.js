8016cdbed93db464957be9bffd378c28111ee4c8
Dotan Simha <dotansimha@gmail.com>
2014-05-16 22:38:02 +0300

    init

---
+++ js-dropdown-multiselect.js
@@ -0,0 +1,53 @@
+/**
+@toc
+
+@param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
+TODO
+
+@param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
+TODO
+
+@dependencies
+TODO
+
+@usage
+partial / html:
+TODO
+
+controller / js:
+TODO
+
+//end: usage
+*/
+
+'use strict';
+
+angular.module('Dotan Simha.angularjs-dropdown-multiselect', []).directive('JsDropdownMultiselect', [ function () {
+
+	return {
+		restrict: 'A',
+		scope: {
+		},
+
+		// replace: true,
+		template: function(element, attrs) {
+			var defaultsAttrs ={
+			};
+			for(var xx in defaultsAttrs) {
+				if(attrs[xx] ===undefined) {
+					attrs[xx] =defaultsAttrs[xx];
+				}
+			}
+			
+			var html ="<div>";
+			html+="</div>";
+			return html;
+		},
+		
+		link: function(scope, element, attrs) {
+		},
+		
+		controller: function($scope, $element, $attrs) {
+		}
+	};
+}]);
---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', function($scope) {
+	//TODO - put any directive code here
+}]);
